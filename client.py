7eeb6a424244a3809348b268e2c01b8d52398f61
SciacovelliMarco <marksciak2014@gmail.com>
2021-02-10 13:28:17 +0100

    L5

--- client.py
+++ client.py
@@ -1,51 +1,52 @@
 #!/usr/bin/env python3
 
-
-input_string = 'Hello'
-print(type(input_string))
-input_bytes_encoded = input_string.encode()
-print(type(input_bytes_encoded))
-print(input_bytes_encoded)
-output_string=input_bytes_encoded.decode()
-print(type(output_string))
-print(output_string)
-
 import socket
+import sys
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 
-sock_service = socket.socket()
+#!La funzione crea una socket una socket (s) per la connessione con il server e la passa alla funzione invia_comandi(s)
+def connessione_server(address, port):
+    sock_service = socket.socket()
+    sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+    print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+    invia_comandi(sock_service)
+    sock_service.close()
 
-sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+#!La funzione riceve la socket connessa al server a la utilizza per richiedere il servizio
+def invia_comandi(socket):
+    while True:
+        try:
+            dati = input("Inserisci i dati da inviare [OPERAZIONE;NUMERO;NUMERO] (0 per uscire): ")
+        except EOFError:
+            print("\nOkay buffone. Exit")
+            break
+        if not dati:
+            print("Non puoi inviare una stringa vuota Tizio Tosto!")
+            continue
+        if dati == '0':
+            print("Chiudo la connessione con il server!")
+            break
+        
+        dati = dati.encode()#trasforma dati in byte
 
-print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
-while True:
-    try:
-        dati = input("Inserisci i dati da inviare [OPERAZIONE;NUMERO;NUMERO] (0 per uscire): ")
-    except EOFError:
-        print("\nOkay buffone. Exit")
-        break
-    if not dati:
-        print("Non puoi inviare una stringa vuota Tizio Tosto!")
-        continue
-    if dati == '0':
-        print("Chiudo la connessione con il server!")
-        break
+        socket.send(dati)
+
+        dati = socket.recv(2048)
+
+        if not dati:
+            print("Server non abbastanza tosto da risponderti. Exit")
+            break
+        
+        dati = dati.decode()
+
+        print("Ricevuto dal server:")
+        print("Risultato operazione: "+dati + '\n')
+
     
-    dati = dati.encode()#trasforma dati in byte
-
-    sock_service.send(dati)
-
-    dati = sock_service.recv(2048)
-
-    if not dati:
-        print("Server non abbastanza tosto da risponderti. Exit")
-        break
-    
-    dati = dati.decode()
-
-    print("Ricevuto dal server:")
-    print("Risultato operazione: "+dati + '\n')
-
-sock_service.close()
+# if __name__ == "__main__": consente al nostro codice di capire se stia venendo eseguito come script a se stante,
+# o se è invece stato richiamato come modulo da un qualche programma per usare una o più delle sue varie
+# funzioni e classi
+if __name__ == "__main__":
+    connessione_server(SERVER_ADDRESS, SERVER_PORT)
--- server.py
+++ server.py
@@ -1,62 +1,63 @@
 #!/usr/bin/env python3
 import socket
 
-
-
 SERVER_ADDRESS = '127.0.0.1'
 
+#Numero di porta deve essere maggiore di 1024 perché le altre sono riservate
 SERVER_PORT = 22224
 
-sock_listen = socket.socket()
+#Crea un endpoint di ascolto (sock_listen) dal quale accettare connessioni in entrata
+#la socket di ascolto viene passata alla funzione ricevi_comandi la quale accettarichieste di connessione
+#e per ognuna crea una socket per i dati (socket service) da cui ricevere le richieste e inviare le risposte
+def avvia_server(address, port):
+    sock_listen = socket.socket()
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock_listen.bind((address, port))
+    sock_listen.listen(5)
+    print("Server in ascolto su %s." % str((address, port)))
+    ricevi_comandi(sock_listen)
 
-sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
-
-sock_listen.listen(5)
-
-print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
-
-
-while True:
-    sock_service, addr_client = sock_listen.accept()
-    print("\nConnessione ricevuta da tizio tosto " + str(addr_client))
-    print("\nAspetto di ricevere i dati da tizio tosto")
-    contConn=0
+def ricevi_comandi(socket):
     while True:
-        dati = sock_service.recv(2048) #ricevo dati
-        contConn+=1
-        
-        if not dati:
-            print("Fine dati dal client. Fine della storia. Reset")
-            break
-        
+        sock_service, addr_client = socket.accept()
+        print("\nConnessione ricevuta da tizio tosto " + str(addr_client))
+        print("\nAspetto di ricevere i dati da tizio tosto")
+        contConn=0
+        while True:
+            dati = sock_service.recv(2048) #ricevo dati
+            contConn+=1
+            
+            if not dati:
+                print("Fine dati dal client. Fine della storia. Reset")
+                break
+            
 
-        dati = dati.decode()
-        print("Ricevuto: '%s'" % dati)
-        if dati=='0':
-            print("Chiudo la connessione con tizio tosto" + str(addr_client))
-            break
-        dati = "Risposta a tizio tosto : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)
+            dati = dati.decode()
+            print("Ricevuto: '%s'" % dati)
+            if dati=='0':
+                print("Chiudo la connessione con tizio tosto" + str(addr_client))
+                break
+            dati = "Risposta a tizio tosto : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)
 
-        dati = dati.split(";")
-        print(dati)
-        ris=0
-        if dati[0]=="più":
-            ris=double(dati[1])+double(dati[2])
-        elif dati[0]=="meno":
-            ris=double(dati[1])-double(dati[2])
-        elif dati[0]=="per":
-            ris=double(dati[1])*double(dati[2])
-        elif dati[0]=="diviso":
-            ris=double(dati[1])/double(dati[2])
-        dati=str(ris)
-        
-        dati = dati.encode()#trasforma i dati in byte
+            dati = dati.split(";")#separa i caratteri che hanno nel mezzo ";" e trasforma la stringa in una lista
+            print(dati)
+            ris=int()
+            #controlla la cella 0 contenente l'operazione da svolgere per poi assegnarla alle celle 1 e 2 ed applicare l'operazione
+            if dati[0]=="più":
+                ris= dati[1] + dati[2]
+            elif dati[0]=="meno":
+                ris= dati[1] - dati[2]
+            elif dati[0]=="per":
+                ris= dati[1] * dati[2]
+            elif dati[0]=="diviso":
+                ris= dati[1] / dati[2]
+            dati=str(ris)
+            dati = dati.encode()#trasforma i dati in byte
+            sock_service.send(dati)#invia i dati
+        sock_service.close()#chiude la trasmissione
 
-        
-
-
-        sock_service.send(dati)#invia i dati
-
-    sock_service.close()#chiude la trasmissione
+# if __name__ == "__main__": consente al nostro codice di capire se stia venendo eseguito come script a se stante,
+# o se è invece stato richiamato come modulo da un qualche programma per usare una o più delle sue varie
+# funzioni e classi
+if __name__ == "__main__":
+    avvia_server(SERVER_ADDRESS, SERVER_PORT)
